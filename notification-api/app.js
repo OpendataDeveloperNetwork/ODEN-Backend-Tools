2324360a72f364031ecd1a74fbf815bf02401823
Steven Chow <steven.chow@live.ca>
2023-05-01 15:21:49 -0700

    Create an email observer module that maintains updated list of AdminEmails on server side.

--- notification-api/app.js
+++ notification-api/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const { getAdminEmails, adminEmailObserver } = require('./emailObserver');
 const { notifyUser } = require('./sendEmail')
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 // TODO - Add routes here.
 
@@ -29,7 +31,19 @@ app.post('/submitDataNotifier', (req, res) => {
     }
 });
 
+/**
+ * This is a temporary route to test whether or not the admin email list gets updated.
+ */
+app.post("/getAdminEmails", (req, res) => {
+    res.json({adminEmailList: getAdminEmails()});
+})
+
+/**
+ * Route to send email notification to Admins of new open data submissions.
+ */
+app.post("/submitDataNotifier", (req, res) => {
+
+});
+
 // Export the app.
 module.exports = app;
-
-
