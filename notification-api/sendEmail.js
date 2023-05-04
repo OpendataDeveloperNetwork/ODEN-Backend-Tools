cf2979d57e48fc14fcf53bb338684a7d68c4e571
Steven Chow <steven.chow@live.ca>
2023-05-04 02:03:18 -0700

    Modify notifyUser to output html & accept subject

--- notification-api/sendEmail.js
+++ notification-api/sendEmail.js
@@ -6,7 +6,7 @@ const password = process.env.PASSWORD;
 /**
  * @summary Notify user if we have received their url submission successfully
  */
-function notifyUser(userEmail, msg) {
+function notifyUser(userEmail, subject, msg) {
   return new Promise((resolve, reject) => {
     var transporter = nodemailer.createTransport({
       service: "gmail",
@@ -21,8 +21,22 @@ function notifyUser(userEmail, msg) {
     const mail_configs = {
       from: adminEmail,
       to: userEmail,
-      subject: 'Data submission notification',
-      text: msg
+      subject: subject,
+      text: msg,
+      html: `<!DOCTYPE html>
+      <html>
+        <body>
+          <header <div style="background-color: #F8F8F8; padding: 20px; font-family: Arial, sans-serif;">
+            <div style="background-color: #ffffff; padding: 10px;">
+              <img src="https://opendatadev.net/assets/Oden_Web_v3_ODEN_Logo.svg" alt="Logo" width="150" height="50" style="display: block; margin: 0 auto;">
+            </div>
+            <h1 style="color: #212121; font-size: 28px; font-weight: bold;">${subject}</h1>
+            <p style="color: #707070; font-size: 16px;"><pre>${msg}</pre></p>
+            <hr style="border: none; border-bottom: 1px solid #E0E0E0; margin: 20px 0;">
+            <p style="color: #BDBDBD; font-size: 14px;">This message was sent automatically. Please do not reply.</p>
+            </div>
+        </body>
+      </html> `
     };
 
     transporter.sendMail(mail_configs, function(error, info){
