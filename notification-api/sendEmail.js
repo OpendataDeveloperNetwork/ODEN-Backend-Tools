c9af942795bb8ff2ccafd489aa653efe8b6ce416
Steven Chow <steven.chow@live.ca>
2023-05-03 23:02:29 -0700

    Refactor code to remove unnecessary logic

--- notification-api/sendEmail.js
+++ notification-api/sendEmail.js
@@ -1,55 +1,41 @@
 const nodemailer = require('nodemailer');
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-
 
 const adminEmail = process.env.EMAIL;
 const password = process.env.PASSWORD;
-// Create a new Express app
-const app = express();
-
-// Set up body-parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 /**
  * @summary Notify user if we have received their url submission successfully
  */
-function notifyUser(userEmail) {
-    return new Promise((resolve, reject) => {
-        var transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-              user: adminEmail,
-              pass: password
-            }
-          })
-        // Set up the email message
-        // Need to specify our email
-        const mail_configs = {
-            from: adminEmail,
-            to: userEmail,
-            subject: 'Data submission notification',
-            text: `
-            Hello user,
-  
-            Your url (data) has been submitted successfully. Our Admin is reviewing it and you will be notified of your submission status as soon as possible.
-            Thank you for your submission!
-  
-            Kind regards,
-            The ODEN team`
-          };
+function notifyUser(userEmail, msg) {
+  return new Promise((resolve, reject) => {
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: adminEmail,
+        pass: password
+      }
+    });
 
-          transporter.sendMail(mail_configs, function(error, info){
-            if(error) {
-                console.log(error)
-                return reject({message: `Email not sent. An error occurred`})
-            }return resolve({message:"Email sent succesfully"})
-          })
-    })
-    }
+    // Set up the email message
+    // Need to specify our email
+    const mail_configs = {
+      from: adminEmail,
+      to: userEmail,
+      subject: 'Data submission notification',
+      text: msg
+    };
 
-    module.exports = {
-      notifyUser
-    }
+    transporter.sendMail(mail_configs, function(error, info){
+      if(error) {
+        console.log(error);
+        reject({message: `Email not sent. An error occurred`});
+      } else {
+        resolve({message:"Email sent succesfully"});
+      }
+    });
+  });
+}
+
+module.exports = {
+  notifyUser
+}
