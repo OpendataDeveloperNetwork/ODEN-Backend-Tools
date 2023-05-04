c8aece9c45d117aee2248a18b4e9957f825349c1
Steven Chow <steven.chow@live.ca>
2023-05-03 23:04:06 -0700

    Refactor post req for new data submissions

--- notification-api/app.js
+++ notification-api/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const axios = require('axios');
-const app = express();
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
 const { getSubscribers } = require('./emailObserver');
 const { notifyUser } = require('./sendEmail')
 
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 
 // TODO - Add routes here.
@@ -19,25 +23,50 @@ app.get('*', function (req, res) {
     res.status(404).send(errorMessage);
 });
 
-app.post('/submitDataNotifier', (req, res) => {
-    console.log(req.body);
-    console.log(req.body.email);
-
-    if (req.body.email) {
-        notifyUser(req.body.email)
-    } else{
-        console
-
-    }
-});
 
 /**
- * Route to send email notification to Admins of new open data submissions.
+ * Route that sends email notification to user(submitter) and admins of newly submitted data sets.
  */
-app.post("/submitDataNotifier", (req, res) => {
-    const adminEmail = getSubscribers().verifyLinkSubscribers;
-    const message = "Notification: Newly submitted open data added to unverified pool."
-    // for each email in adminEmail, send message using nodemailer function...
+app.post('/submitDataNotifier', (req, res) => {
+    console.log(`User email posted to /submitDataNotifier: ${req.body.email}`);
+
+    userMessage = `
+    Hello user,
+
+    Your open data information has been submitted successfully. Our Admins are reviewing it and you will be notified of your submission status as soon as possible.
+    Thank you for your submission!
+
+    Kind regards,
+    The ODEN team`;
+
+    adminMessage = `
+    Hello admins,
+    
+    A user has submitted new data, please login and approve/reject the dataset.
+    
+    Kind regards,
+    The ODEN team`;
+
+    if (req.body.email) {
+        notifyUser(req.body.email, userMessage).then((result) => {
+            console.log(result.message);
+        }).catch((error) => {
+            console.error(error.message);
+        });
+    } else {
+        console.log("/submitDataNotifier received no email...");
+    }
+
+    // Get list of admins 
+    const adminEmails = getSubscribers().adminVerifyLinks;
+    if (!adminEmails.length) {
+        console.log("/submitDataNotifier has no Admins subscribed to receiver new data submission.");
+    }
+    // Send an email to each administrator that has subscribed to receive notifications of newly submitted open data.
+    adminEmails.forEach(email => {
+        notifyUser(email, adminMessage);
+        console.log(`/submitDataNotifier send notification to admin: ${email}`);
+    });
 });
 
 // Export the app.
